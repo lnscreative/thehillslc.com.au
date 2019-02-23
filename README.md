98b0f0ec9a766fb4dbebb7ee088ddd539547d12f
Luke Goodwin <luke.goodwin@chook.com.a>
2019-02-23 12:43:11 +1100

    Start Build

---
+++ README.md
@@ -0,0 +1,1 @@
+thehillslc.com.au
---
+++ thehillslc.com.au/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace thehillslc.com.au.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
